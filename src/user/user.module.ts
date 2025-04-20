54b4c5554de672e4c5a985a336a19f547ec6efed
frank <fugangkuang4@gmail.com>
2025-04-20 22:56:37 +0800

    ✨ feat: user模块使用pino日志输出

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,68 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Logs } from 'src/logs/logs.entity';
+import { LoggerModule } from 'nestjs-pino';
 
+const isDev = process.env.NODE_ENV === 'development';
 @Module({
   // 如果没有配置ConfigModule为全局模块，则在其他模块中使用ConfigService时需要导入ConfigModule
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User, Logs]), // 在当前范围中注册User、Logs存储库
+    LoggerModule.forRoot({
+      pinoHttp: {
+        // transport: {
+        //   targets: [
+        //     {
+        //       level: 'info', // 只记录info级别以上的日志
+        //       target: 'pino-pretty',
+        //       options: {
+        //         colorize: true,
+        //         translateTime: 'SYS:standard',
+        //         // ignore: 'pid,hostname',
+        //       },
+        //     },
+        //     {
+        //       level: 'info', // 只记录info级别以上的日志
+        //       target: 'pino-roll',
+        //       options: {
+        //         file: './logs/log.txt',
+        //         frequency: 'daily', // 日志文件滚动频率
+        //         mkdir: true,
+        //         size: '10M', // 每个日志文件的大小限制
+        //         compress: false, // 是否压缩日志文件
+        //         // 过期时间，超过这个时间的日志文件将被删除
+        //         // maxFiles: '30d', // 30天前的日志文件将被删除
+        //       },
+        //     },
+        //   ],
+        // },
+        transport: isDev
+          ? {
+              // 一般在开发环境下才使用pino-pretty
+              target: 'pino-pretty', //使用pino-pretty格式化日志
+              options: {
+                colorize: true, //启用颜色
+                translateTime: 'SYS:standard', //将时间戳转换为标准格式
+                // ignore: 'pid,hostname', //忽略pid和hostname字段
+              },
+            }
+          : {
+              // 生产环境下使用默认的JSON格式
+              level: 'info',
+              target: 'pino-roll',
+              options: {
+                file: 'log.txt', // 日志文件路径
+                frequency: '1d', // 日志文件滚动频率
+                mkdir: true, // 如果目录不存在则创建
+              },
+            },
+      },
+    }), // 注册pino日志模块
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,78 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { Logs } from 'src/logs/logs.entity';
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return { code: 0, data: [], msg: '请求成功' };
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Logs) private readonly logsRepository: Repository<Logs>,
+  ) {}
+
+  findAll() {
+    // 查询所有用户
+    return this.userRepository.find();
   }
-  addUser() {
-    return { code: 0, data: [], msg: '添加用户成功' };
+  find(username: string) {
+    // 根据用户名查询用户
+    return this.userRepository.findOne({
+      where: { username },
+    });
+  }
+
+  create(user: User) {
+    // 创建 User 的新实例
+    const temp = this.userRepository.create(user);
+    // 保存给定的实体或实体数组
+    return this.userRepository.save(temp);
+  }
+
+  update(id: number, user: Partial<User>) {
+    // 更新用户信息
+    return this.userRepository.update(id, user);
+  }
+
+  remove(id: number) {
+    // 删除用户
+    return this.userRepository.delete(id);
+  }
+
+  findProfile(id: number) {
+    // 查询用户的同时查询个人资料
+    return this.userRepository.findOne({
+      where: { id },
+      relations: {
+        profile: true,
+      },
+    });
+  }
+
+  findLogs(id: number): any {
+    // 查询用户
+    return this.logsRepository.find({
+      where: {
+        user: { id },
+      },
+      relations: {
+        user: true,
+      },
+    });
+  }
+
+  findLogsByGroup(id: number): any {
+    // 使用原生SQL查询
+    // return this.logsRepository.query(
+    //   `SELECT result, COUNT(result) as count from logs where user_id=${id} GROUP by result`,
+    // );
+    // 使用QueryBuilder查询
+    return this.logsRepository
+      .createQueryBuilder('logs')
+      .select('result')
+      .addSelect('COUNT(result) as count')
+      .where('user_id = :id', { id }) //防止SQL注入
+      .groupBy('result')
+      .getRawMany();
   }
 }
