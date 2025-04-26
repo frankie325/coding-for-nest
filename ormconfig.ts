f74ad94b51e06a0ce4894fbac0acebb74ce7a9e3
frank <fugangkuang4@gmail.com>
2025-04-26 10:36:48 +0800

    ✨ feat: 通过环境变量获取配置信息

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,22 +1,42 @@
 import 'reflect-metadata';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { User } from './src/user/user.entity';
-import { Profile } from './src/user/profile.entity';
-import { Roles } from './src/roles/roles.entity';
-import { Logs } from './src/logs/logs.entity';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import { ConfigEnum } from 'config/enum';
 
-export const connectionParams = {
-  type: 'mysql',
-  host: '127.0.0.1',
-  port: 3307,
-  username: 'root',
-  password: '123456',
-  database: 'nest',
-  synchronize: true,
-  logging: false,
-  entities: [User, Profile, Roles, Logs],
-} as TypeOrmModuleOptions;
+function getEnv(env: string): Record<string, any> {
+  if (fs.existsSync(env)) {
+    return dotenv.parse(fs.readFileSync(env));
+  }
+  return {};
+}
+
+// 根据环境变量加载配置
+function buildConnectOptions() {
+  const defaultConfig = getEnv('.env');
+  const envConfig = getEnv(`.env.${process.env.NODE_ENV}`);
+  const config = { ...defaultConfig, ...envConfig };
+
+  const entitiesDir =
+    process.env.NODE_ENV === 'test'
+      ? [__dirname + '**/*.entity.ts']
+      : [__dirname + '/**/*.entity{.js,.ts}'];
+
+  return {
+    type: config[ConfigEnum.DATABASE_TYPE] as string,
+    host: config[ConfigEnum.DATABASE_HOST] as string,
+    port: config[ConfigEnum.DATABASE_PORT] as number,
+    username: config[ConfigEnum.DATABASE_USER] as string,
+    password: config[ConfigEnum.DATABASE_PASSWORD] as string,
+    database: config[ConfigEnum.DATABASE_NAME] as string,
+    synchronize: true,
+    logging: false,
+    entities: entitiesDir,
+  } as TypeOrmModuleOptions;
+}
+
+export const connectionParams = buildConnectOptions();
 
 export const AppDataSource = new DataSource({
   ...connectionParams,
